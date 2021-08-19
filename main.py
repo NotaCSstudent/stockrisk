d809d734adeb48a52f41c44f106f1981e52292ee
NotaCSstudent <mahmudbabu94@gmail.com>
2021-08-19 17:23:53 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -9,40 +9,47 @@ import matplotlib.pyplot as plt
 
 load_dotenv(dotenv_path='./stuff.env')
 
-token = os.getenv('TOKEN')
+
 
 
 import requests
 import pandas as pd
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=TSLA&interval=5min&apikey={token}'
-r = requests.get(url)
-data = r.json()
-
-#print(data)
-TSLA = pd.DataFrame(data)
-#print(TSLA)
-
-TSLAmin = data['Time Series (5min)']
-
-more = pd.DataFrame(TSLAmin)
-#print(more)
-
-price = []
-timeperiod = []
-for keys in TSLAmin:
-    price.append(TSLAmin[keys]['1. open'])
-    timeperiod.append(keys)
-
-#print(timeperiod)
-
 import numpy as np
 
+token = os.getenv('TOKEN');
+class Ticker:
+    def __init__(self,ticker:str):
+        
+        self.ticker = ticker;
+        self.daily = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ self.ticker + '&apikey={token}').json();
+        self.intraday = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+ self.ticker +'&interval=5min&apikey={token}').json();
+        self.monthly = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol='+ self.ticker +'&apikey={token}').json();
+        self.daily_data = {'date' : [],'open' : [], 'close' : [],'high' : [], 'low' : [] , 'volume' : []}
+    def dailydata(self):
+        daydata  = self.daily['Time Series (Daily)']
+        for keys in daydata:
+            self.daily_data['date'].append(keys);
+            self.daily_data['open'].append(float(daydata[keys]['1. open']))
+            self.daily_data['high'].append(float(daydata[keys]['2. high']))
+            self.daily_data['low'].append(float(daydata[keys]['3. low']))
+            self.daily_data['close'].append(float(daydata[keys]['4. close']))
+            self.daily_data['volume'].append(int(daydata[keys]['5. volume']))
+        
+        return self.daily_data
 
-y = np.array(price)
-x = timeperiod
+
+        
+        
 
 
-plt.plot(x,y)
+        
+        
 
 
+TSLA = Ticker('TSLA')
+
+
+
+data = TSLA.dailydata()
+#print(data)

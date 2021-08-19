3bba115109aa64ded172ef5eadbc9e8da7c895fe
NotaCSstudent <mahmudbabu94@gmail.com>
2021-08-19 14:29:51 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+#   Project details
+
+In this project I will be attempting to figure out how risky a stock can be, i will be using real time data on alpha vantage and using mathematical models to detect how risky a stock can be. This will be a long term project since I am currently in school learning this.
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from datetime import time
 import dotenv as env
 from dotenv import load_dotenv
 import os
+import matplotlib
+import matplotlib.pyplot as plt
+
+
 
 load_dotenv(dotenv_path='./stuff.env')
 
@@ -8,10 +13,36 @@ token = os.getenv('TOKEN')
 
 
 import requests
-
+import pandas as pd
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=TSLA&interval=5min&apikey={token}'
 r = requests.get(url)
 data = r.json()
-print(type(data))
+
 #print(data)
+TSLA = pd.DataFrame(data)
+#print(TSLA)
+
+TSLAmin = data['Time Series (5min)']
+
+more = pd.DataFrame(TSLAmin)
+#print(more)
+
+price = []
+timeperiod = []
+for keys in TSLAmin:
+    price.append(TSLAmin[keys]['1. open'])
+    timeperiod.append(keys)
+
+#print(timeperiod)
+
+import numpy as np
+
+
+y = np.array(price)
+x = timeperiod
+
+
+plt.plot(x,y)
+
+
